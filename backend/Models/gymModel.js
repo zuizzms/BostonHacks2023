2ec21ed82f6390859a1281979e58ddfa711eddcc
Jeffrey Zhou <jeffreyzhou@crc-dot1x-nat-10-239-102-193.bu.edu>
2023-11-18 16:54:32 -0500

    schema

---
+++ backend/Models/gymModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema
+
+const gymSchema = new Schema({
+    gymName: {
+        type: String,
+        required: truek
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    numRatings: {
+        type: Number,
+        required: true
+    },
+    lgbtRating: {
+        type: Number,
+        required: true
+    },
+    genderRating: {
+        type: Number,
+        required: true
+    },
+    ageRating: {
+        type: Number,
+        required: true
+    }
+
+})
+
+module.exports = mongoose.model('Gym', gymSchema)
