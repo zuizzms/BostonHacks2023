d6b23e44043e1ef5f144ad374c30e869a5c80d95
Jeffrey Zhou <46510337+JeffreyZhou25@users.noreply.github.com>
2023-11-19 08:51:43 -0500

    all frontend-backend routing finished

--- frontend/src/components/gym/Gym.js
+++ frontend/src/components/gym/Gym.js
@@ -2,14 +2,17 @@ import React, {useEffect} from "react";
 import styled from "styled-components";
 import {NavLink} from "react-router-dom";
 import "./gym.css";
-import {useState} from 'react';
 import { useParams } from "react-router-dom";
 import useSWR from 'swr';
 
+const fetcher = url => fetch(url).then(r => r.json())
+
 function Gym() {
     const { documentID } = useParams();
-    const url = 'http://localhost:8080/Gym/' + documentID;
-    const {data} = useSWR('/', () => fetch('http://localhost:8080/Gym/').then(res => res.json()))
+    const url = 'http://localhost:8080/api/Gym/' + documentID;
+    // const {data} = useSWR('/', () => fetch(url).then(res => res.json()))
+    const {data} = useSWR(url, fetcher)
+
     // const Home = () => {
     //     const { documentID } = useParams();
     //     const [data, setData] = useState(null);
@@ -21,12 +24,16 @@ function Gym() {
     //         .then((data) => setData(data));
     // }, []);
 
+    if (!data) {
+        return null
+    }
+
     return(
         <div className='gym-div'> 
              <div className = " in-content">
                 <div className = "gym-header">
-                    <h1>Gym Name Placeholder</h1>
-                    <h3>Gym Address Placeholder</h3>
+                    <h1>{data.gymName}</h1>
+                    <h3>{data.address}</h3>
                 </div>
                 <div className = "gym-body">
                     <div className = "rating">
