12fa12bf12e33b28f8ff784088bd6bd3626fbb00
devnvanzandt@gmail.com <devnvanzandt@gmail.com>
2023-11-19 02:14:28 -0500

    IDK

--- frontend/src/components/rating/Rating.js
+++ frontend/src/components/rating/Rating.js
@@ -7,9 +7,10 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
-  margin: 80%;
+  align-items: center;
+  width: 100%;
 `
-function Rating(gymName){
+function Rating(){
 
     const[lgbtqRating, setLGBTRating] = useState('');
     const[genderRating, setGenderRating] = useState('');
@@ -18,7 +19,7 @@ function Rating(gymName){
     return(
         <Wrapper>
             <div className = "rating-header">
-                <h2>{gymName}</h2>
+                <h2></h2>
                 <h4>Rate this gym according to these categories!</h4>
             </div>
             <div className = "rating-body">
