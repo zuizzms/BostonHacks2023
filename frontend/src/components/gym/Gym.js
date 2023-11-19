3f9a12ae369e35488ac978a6d6c941c8e3beb057
Matthew Berk <mberk@bu.edu>
2023-11-19 03:48:50 -0500

    styled

--- frontend/src/components/gym/Gym.js
+++ frontend/src/components/gym/Gym.js
@@ -3,14 +3,17 @@ import styled from "styled-components";
 import {NavLink} from "react-router-dom";
 import "./gym.css";
 import { useParams } from "react-router-dom";
+import useSWR from "swr";
+import result from "../result/Result";
+import Result from "../result/Result";
 
 function Gym(){
     const { documentID } = useParams();
-
+    const {data} = useSWR('/gyms', () => fetch(`http://localhost:8080/gyms`).then(res => res.json()))
     
     return(
         <div className='gym-div'> 
-             <div className = "main-content">
+             <div className = "main-content2">
                 <div className = "gym-header">
                     <h1></h1>
                     <h3></h3>
